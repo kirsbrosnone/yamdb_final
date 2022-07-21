dd813eb63cc393b14c9d2a1f76e52de820bde0df
Кирсанов Роман <romat42ru@gmail.com>
2022-07-22 00:58:25 +0700

    some fixes

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -15,12 +15,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '84.201.178.60',
-    'localhost',
-    '127.0.0.1',
-    'web',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
